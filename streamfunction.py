8fefe4fd73d17b69524cef6dd2b80cec90d3a70f
emiliolr <emilio.luz.ricca@gmail.com>
2024-02-14 23:00:18 +0000

    Some streamfunction thing that looks kind of promising

--- streamfunction.py
+++ streamfunction.py
@@ -16,6 +16,7 @@ warnings.filterwarnings("ignore")
 
 from functools import reduce
 
+from basin_masks import *
 
 def _format_lat_lon(value):
     """Format latitude or longitude with N/S or E/W suffix."""
@@ -160,6 +161,88 @@ def plot_2D_streamfunction(stf_ds, title=None):
     plt.grid(True)
     plt.show()
 
+def get_gridllc0090_mask(target_latitude, longitudes, ds):
+
+    FLIPPED_TILES = [7,8,9,10,11,12,13]
+    
+    # Split up the separate sections for the longitude list
+    longitude_sections = np.split(longitudes, np.where(np.diff(longitudes) > 1)[0] + 1)
+    
+    # Create an empty mask
+    mask = xr.DataArray(data=0, dims=ds['YC'].dims, coords=ds['YC'].coords)
+
+    # Iterate over each "tile" of the grid 
+    for tile_num in ds['tile'].values:
+
+        if tile_num+1 in FLIPPED_TILES:
+            latitude_dim = 'j'
+        else:
+            latitude_dim = 'i'
+
+        tile_lats = ds['YC'].sel(tile=tile_num)
+        tile_lons = ds['XC'].sel(tile=tile_num)
+    
+        tile_mask = abs(tile_lats - target_latitude) 
+        zeros = xr.zeros_like(tile_mask)
+    
+        first_col = tile_lats.isel(**{latitude_dim: 0})
+        lowest_lat = first_col.values.max()
+        highest_lat = first_col.values.min()
+    
+        if max(lowest_lat, highest_lat) > target_latitude and min(lowest_lat, highest_lat) < target_latitude:
+        
+            row_to_mask = int(tile_mask.isel(**{latitude_dim: 0}).argmin().values)
+        
+            for longitude in longitude_sections:
+                if tile_num+1 in FLIPPED_TILES:
+                    column_mask = (tile_lons[:, row_to_mask] >= longitude[0]) & (tile_lons[:, row_to_mask] <= longitude[-1])
+                    zeros.loc[column_mask, row_to_mask]= 1
+                else:
+                    column_mask = (tile_lons[row_to_mask, :] >= longitude[0]) & (tile_lons[row_to_mask,:] <= longitude[-1])
+                    zeros.loc[row_to_mask, column_mask]= 1
+    
+        # Update the mask DataArray for the current face
+        mask.loc[dict(tile=tile_num)] = zeros.astype(int)
+    return mask
+
+def calculate_streamfunction_JANKY(ds, basin, latitude, time, geom_fp):
+    longitudes = basin_masks.get_longitudes_at_latitude(latitude, basin, geom_fp)
+
+    mask = get_gridllc0090_mask(latitude, longitudes, ds)
+
+    velocity = ds['VVELMASS'].isel(time = time)
+    delta_x = ds['dxG']
+    delta_z = ds['drF']
+
+    weighted_velocity = velocity * delta_x * delta_z
+
+    mask = mask.rename({'j' : 'j_g'}) 
+    weighted_velocity_at_lat = mask * weighted_velocity
+
+    lon_by_depth = []
+    for i in range(13):    
+        arr = weighted_velocity_at_lat.isel(tile = i).fillna(0).values
+        nonzero_ind = np.nonzero(arr)
+
+        unique_vals = np.unique(nonzero_ind[0]), np.unique(nonzero_ind[1])
+        if len(unique_vals[0]) == 1:
+            nonzero_vals = arr[unique_vals[0][0], : , : ]
+        elif len(unique_vals[1]) == 1:
+            nonzero_vals = arr[ : , unique_vals[1][0], : ]
+        else:
+            print('No nonzero vals')
+            continue
+
+        print(nonzero_vals.shape)
+        lon_by_depth.append(nonzero_vals)
+
+    lon_by_depth = np.concatenate(lon_by_depth, axis = 0)
+    streamfunction = -1 * lon_by_depth.sum(axis = 0)[ : : -1].cumsum()
+    depth_level = np.abs(streamfunction).argmax()
+
+    strength_sv = round(streamfunction[depth_level] / (10 ** 6))
+
+    return strength_sv
 
 if __name__ == "__main__":
 
