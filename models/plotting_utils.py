f42d0812c841f431eacb02e1b4d4c78c7ac87250
emiliolr <emilio.luz.ricca@gmail.com>
2024-02-20 23:44:23 +0000

    Added plotting func for scatterplot

--- models/plotting_utils.py
+++ models/plotting_utils.py
@@ -53,3 +53,56 @@ def timeseries_comparison(pred, actual, time, len_xtrain, numerical_model = 'ECC
     ax.set_ylabel('MOC Strength [Sv]', weight = 'bold')
 
     return fig, ax
+
+def pred_vs_actual(train_pred, test_pred, train_label, test_label, numerical_model = 'ECCO', figsize = (4, 4)):
+
+    """
+    Plotting the results for predicted vs. actual MOC predictions as a scatterplot
+
+    Parameters
+    ----------
+    train_pred : numpy.array
+        array of MOC predictions on the train set
+    test_pred : numpy.array
+        array of MOC predictions on the test set
+    train_label : numpy.array
+        array of MOC model values for the train set
+    test_pred : numpy.array
+        array of MOC model values for the test set
+    numerical_model : string
+        the numerical model used, either ECCO or ACCESS
+    figsize : tuple
+        the size of the figure
+
+    Returns
+    -------
+    fig, ax : matplotlib.figure, matplotlib.axes
+        figure and axis objects from matplotlib
+    """
+
+    # Set up figure and axes to return later
+    fig, ax = plt.subplots(figsize = figsize)
+
+    #  computing (linear) correlation for train and test set
+    train_corr = round(np.corrcoef(train_pred, train_label)[0, 1], 3)
+    test_corr = round(np.corrcoef(test_pred, test_label)[0, 1], 3)
+
+    #  adding 1:1 line
+    ax.axline((0, 0), slope = 1, color = 'black', linestyle = '--', zorder = 0)
+
+    #  adding scatterplot for train and test set separately
+    ax.scatter(train_pred, train_label, alpha = 0.4, color = 'blue', s = 15, label = f'Train (Cor.={train_corr})', zorder = 20)
+    ax.scatter(test_pred, test_label, alpha = 0.4, color = 'orange', s = 15, label = f'Test (Cor.={test_corr})', zorder = 20)
+
+    #  adding legend
+    ax.legend(loc = 'upper left', edgecolor = 'black', framealpha = 1)
+
+    #  ensuring the axes limits are the same and adding grid
+    ax.axis('square')
+    ax.grid(alpha =  0.3)
+
+    #  labeling axes
+    ax.set_xlabel('Predicted MOC Strength [Sv]')
+    ax.set_ylabel(f'{numerical_model} MOC Strength [Sv]')
+
+    return fig, ax
