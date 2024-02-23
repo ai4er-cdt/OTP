55022074197f80efc549aa460e3865ba2a7b47f3
AlineGenvision <aline@genvision.com>
2024-02-23 09:40:51 +0000

    Notebook to train neural networks: -1D CNN - slightly modified train function to unsqueeze when there are not enough dimensions to fit as tensor

--- scripts/archive/download_flux.py
+++ scripts/archive/download_flux.py
@@ -7,12 +7,12 @@ shortname = "ECCO_L4_OCEAN_3D_VOLUME_FLUX_LLC0090GRID_MONTHLY_V4R4"
 start = "1992-01-01"
 end = "2017-12-31"
 
-'''
+"""
 home = paths.SOLODOCH_DATA
 shortname = "ECCO_L4_BOLUS_LLC0090GRID_MONTHLY_V4R4"
 start = "1992-01-01"
 end = "2018-01-01"
-'''
+"""
 
 
 ecco_podaac_download_subset(
