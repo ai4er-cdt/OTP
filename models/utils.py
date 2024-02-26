5fbee8e7fea8cd91d54f1dd66becb5f45dcd774d
emiliolr <emilio.luz.ricca@gmail.com>
2024-02-26 18:18:12 +0000

    Updated linear regression stuff with density space and fixed preprocessing code

--- models/utils.py
+++ models/utils.py
@@ -2,8 +2,8 @@ from typing import Callable, Any, List, Literal, Tuple, Optional
 import numpy as np
 from numpy.lib.stride_tricks import as_strided
 import xarray as xr
-from scipy.signal import butter, filtfilt 
-from statsmodels.tsa.seasonal import seasonal_decompose 
+from scipy.signal import butter, filtfilt
+from statsmodels.tsa.seasonal import seasonal_decompose
 from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import GridSearchCV, train_test_split
@@ -11,9 +11,9 @@ from sklearn.metrics import r2_score, mean_squared_error
 import torch as t
 
 
-def reshape_inputs(data: xr.core.dataset.Dataset, 
+def reshape_inputs(data: xr.core.dataset.Dataset,
                    keep_coords: List=["time", "latitude", "longitude"],
-                   avg_time_window: Optional[int]=None, 
+                   avg_time_window: Optional[int]=None,
                    history: Optional[int]=None,
                    data_vars: List=["SSH", "SST", "SSS", "OBP", "ZWS"],
                    return_pt: bool=False) -> np.ndarray | t.Tensor:
@@ -31,7 +31,7 @@ def reshape_inputs(data: xr.core.dataset.Dataset,
     data_vars: data variables to be kept.
     return_pt: if true, returns a pytorch tensor (cpu!) instead of a numpy array.
     """
-    
+
     def moving_average(data: np.ndarray,
                        lag: int) -> np.ndarray:
         """
@@ -47,37 +47,37 @@ def reshape_inputs(data: xr.core.dataset.Dataset,
         s = data.strides; strides = (s[0], s[0], s[1], s[2], s[3])
         data_ = as_strided(data, shape=view_shape, strides=strides)
         return data_.mean(axis=1).squeeze(axis=1)
-    
+
     coords = ["time", "latitude", "longitude"]
     data = data[coords + data_vars].to_array().values
     data = data.transpose(1, 2, 3, 0)
     for ax in coords:
-        if ax not in keep_coords: 
-            if ax == "time" and avg_time_window != None: 
+        if ax not in keep_coords:
+            if ax == "time" and avg_time_window != None:
                 data = moving_average(data, avg_time_window)
             else:
                 data = data.mean(axis=coords.index(ax))
-            coords = [c for c in coords if c != ax]   
+            coords = [c for c in coords if c != ax]
 
     if history != None:
-        if "time" not in keep_coords: raise Exception("Error. 'time' must be in keep_coords in order to use history.")  
+        if "time" not in keep_coords: raise Exception("Error. 'time' must be in keep_coords in order to use history.")
         coords = ["time", "history"] + coords[1:]
         n_times = data.shape[0]
         if history > n_times: raise ValueError("Desired history is longer than the full time series.")
-        view_shape = (n_times-history+1, history, *data.shape[1:])      
+        view_shape = (n_times-history+1, history, *data.shape[1:])
         s = data.strides[0]
-        data = as_strided(data, shape=view_shape, strides=(s, s, *data.strides[1:]))    
+        data = as_strided(data, shape=view_shape, strides=(s, s, *data.strides[1:]))
 
     print(f"axes: {coords + ['feature']}")
     print(f"variables: {data_vars}")
-    print(f"shape: {data.shape}")    
+    print(f"shape: {data.shape}")
     return t.Tensor(data) if return_pt else data
 
 def apply_preprocessing(dataset, mode = 'inputs', remove_trend = True, remove_season = True, standardize = True, lowpass = False):
 
     """
     Preprocessing function for covariates, including de-trending, de-seasonalizing, standardization,
-    and applying a low-pass filter to remove effect of short timescale variations. All operations 
+    and applying a low-pass filter to remove effect of short timescale variations. All operations
     act on the longitudinal timeseries and for each covariate independently.
 
     Parameters
@@ -91,7 +91,7 @@ def apply_preprocessing(dataset, mode = 'inputs', remove_trend = True, remove_se
     remove_season : boolean
         should the seasonal trend be removed?
     standardize : boolean
-        should we apply standardization, i.e., subtract time-mean and divide by standard 
+        should we apply standardization, i.e., subtract time-mean and divide by standard
         deviation?
     lowpass : boolean
         should we apply a low-pass filter to the covariate timeseries?
@@ -99,25 +99,31 @@ def apply_preprocessing(dataset, mode = 'inputs', remove_trend = True, remove_se
     Returns
     -------
     preprocessed_array : xarray.dataset
-        xarray dataset with the same format as input, but with preprocessed covariates 
+        xarray dataset with the same format as input, but with preprocessed covariates
     """
 
     avail_modes = ['inputs', 'outputs']
     assert mode in avail_modes, f'mode argument must be one of {avail_modes}'
 
+    if mode == 'inputs':
+        dims = ('time', 'latitude', 'longitude')
+
+        if 'latitude' not in dataset.dims:
+            dims = (dims[0], dims[2])
+    elif mode == 'outputs':
+        dims = ('time', 'latitude')
+
+        if 'latitude' not in dataset.dims:
+            dims = (dims[0], )
+
     # Making sure the dataset has the expected ordering or coordinates
     if mode == 'inputs':
-        dataset = dataset.transpose('time', 'latitude', 'longitude')
+        dataset = dataset.transpose(*dims)
     elif mode == 'outputs':
-        dataset = dataset.transpose('time', 'latitude')
+        dataset = dataset.transpose(*dims)
 
     # Instantiating a new array like the original to populate with preprocessed values
     preprocessed_array = xr.full_like(dataset, 0)
-
-    if mode == 'inputs':
-        dims = ('time', 'latitude', 'longitude')
-    elif mode == 'outputs':
-        dims = ('time', 'latitude')
 
     for k in dataset.keys():
         var = dataset[k].values.squeeze()
@@ -144,9 +150,9 @@ def apply_preprocessing(dataset, mode = 'inputs', remove_trend = True, remove_se
             new_var = scaler.fit_transform(new_var)
 
         # Adding back in latitude dimension that got squeezed out
-        if mode == 'inputs':
+        if mode == 'inputs' and 'latitude' in dataset.dims:
             new_var = new_var.reshape(new_var.shape[0], 1, new_var.shape[1])
-        elif mode == 'outputs':
+        elif mode == 'outputs' and 'latitude' in dataset.dims:
             new_var = new_var.reshape(new_var.shape[0], 1)
 
         preprocessed_array[k] = (dims, new_var)
@@ -156,7 +162,7 @@ def apply_preprocessing(dataset, mode = 'inputs', remove_trend = True, remove_se
 def align_inputs_outputs(inputs, outputs, date_range = ('1992-01-16', '2015-12-16'), ecco = True):
 
     """
-    Align input and output dataset date ranges and latitudes to prepare for preprocessing. 
+    Align input and output dataset date ranges and latitudes to prepare for preprocessing.
     If working with ECCO, this will also extract the MOC strength variable.
 
     Parameters
@@ -175,12 +181,31 @@ def align_inputs_outputs(inputs, outputs, date_range = ('1992-01-16', '2015-12-1
     inputs, outputs : xarray.dataset, xarray.dataset
         aligned input and output datasets
     """
-    
+
     inputs = inputs.sel(time = slice(*date_range))
     outputs = outputs.sel(time = slice(*date_range))
-    
+
     if ecco:
         outputs = outputs.moc.to_dataset()
         outputs = outputs.rename({'lat' : 'latitude'})
 
     return inputs, outputs
+
+if __name__ == '__main__':
+    data_home = "/Users/emiliolr/Google Drive/My Drive/GTC"
+    lats = ["26N", "30S", "55S", "60S"]
+    lat = lats[3]
+    inputs = xr.open_dataset(f"{data_home}/ecco_data_minimal/{lat}.nc")
+    inputs = inputs.sel(latitude = -59.75) # pull out just 60S
+
+    remove_season = True
+    remove_trend = True
+
+    pp_data = apply_preprocessing(inputs,
+                                  mode="inputs",
+                                  remove_season=remove_season,
+                                  remove_trend=remove_trend,
+                                  standardize=True,
+                                  lowpass=False)
+
+    print(pp_data)
