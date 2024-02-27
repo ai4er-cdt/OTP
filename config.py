e468553b2af1871d36a4b4c88aa2ecf6abc2046d
ninagw3 <ninawingate@yahoo.co.uk>
2024-02-27 16:13:28 +0000

    Update neural network file for training models

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # for Sharan: "/mnt/g/My Drive/GTC/
 # for Emilio: "/Users/emiliolr/Google Drive/My Drive/GTC/"
-LOCAL_DIR = "H:/.shortcut-targets-by-id/1wvJjD0RMTujKYaXQapEiGk-Mx03_KSin/GTC"
+LOCAL_DIR = "G:/.shortcut-targets-by-id/1wvJjD0RMTujKYaXQapEiGk-Mx03_KSin/GTC"
