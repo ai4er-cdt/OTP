9b7901feb168551ecae32c3ff7f9d91a8d54ad5a
AlineGenvision <aline@genvision.com>
2024-03-08 23:09:33 +0000

    Formatting notebooks: - neural networks - RAPID_transfer_linear_regression + cleanup function streamfunction_custom_utils

--- streamfunction_ecco/streamfunction_custom_utils.py
+++ streamfunction_ecco/streamfunction_custom_utils.py
@@ -3,14 +3,6 @@ import xarray as xr
 import matplotlib.pyplot as plt
 
 
-def format_lat_lon(value):
-    """Format latitude or longitude with N/S or E/W suffix."""
-    if value < 0:
-        return f"{abs(value)}S"
-    else:
-        return f"{value}N"
-
-
 def get_gridllc0090_mask(ds, target_latitude, longitudes):
     FLIPPED_TILES = [7, 8, 9, 10, 11, 12, 13]
 
@@ -60,51 +52,6 @@ def get_gridllc0090_mask(ds, target_latitude, longitudes):
         # Update the mask DataArray for the current face
         mask.loc[dict(tile=tile_num)] = zeros.astype(int)
     return mask
-
-
-def get_PSI_at_max_density_level(PSI, moc_param="psi_moc", max=True):
-    PSI_mean = PSI[moc_param].mean("time")  # np.abs()
-    if max is True:
-        max_index = PSI_mean.argmax(dim="k")
-    else:
-        max_index = PSI_mean.argmin(dim="k")
-    return PSI.isel(k=max_index)
-
-
-def plot_depth_stf_vs_time(stf_ds, label, param):
-    fig = plt.figure(figsize=(18, 6))
-
-    # Time evolving
-    plt.subplot(1, 4, (1, 3))
-    time_edge_extrap = np.hstack(
-        (
-            stf_ds["time"].values[0] - (0.5 * np.diff(stf_ds["time"].values[0:2])),
-            stf_ds["time"].values[:-1] + (0.5 * np.diff(stf_ds["time"].values)),
-            stf_ds["time"].values[-1] + (0.5 * np.diff(stf_ds["time"].values[-2:])),
-        )
-    )
-    Z_edge_extrap = np.hstack(
-        (
-            np.array([0]),
-            stf_ds["Z"].values[:-1] + (0.5 * np.diff(stf_ds["Z"].values)),
-            np.array([-6134.5]),
-        )
-    )
-    plt.pcolormesh(time_edge_extrap, Z_edge_extrap, stf_ds[param].T)
-    plt.title("ECCOv4r4\nOverturning streamfunction across latitude %s [Sv]" % label)
-    plt.ylabel("Depth [m]")
-    plt.xlabel("Month")
-    plt.xticks(rotation=45)
-    cb = plt.colorbar()
-    cb.set_label("[Sv]")
-
-    plt.subplot(1, 4, 4)
-    plt.plot(stf_ds[param].mean("time"), stf_ds["Z"])
-    plt.title("ECCOv4r4\nTime mean streamfunction %s" % label)
-    plt.ylabel("Depth [m]")
-    plt.xlabel("[Sv]")
-    plt.grid()
-    plt.show()
 
 
 def plot_2D_streamfunction(stf_ds, moc_param="psi_moc", title=None):
